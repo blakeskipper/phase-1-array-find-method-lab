16f987ad59c0cae2b77696f013391503c1997c39
blakeskipper <blakeskipper@gmail.com>
2022-03-20 00:00:10 -0400

    commit tree c216ff4f9fbad38e0fc6384293a725077adb0961

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+    const foundSeason = record.find(function(season){
+        return season.result === "W"
+    })
+    if (foundSeason){
+    return foundSeason.year
+    }
+}
